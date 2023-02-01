3409c004ade3febde6e4361c7f16b4f1108a081b
steiner <steiner3044@163.com>
2023-02-02 00:08:47 +0800

    adjust project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from "vite-plugin-dts"
+import {resolve} from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts()],
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
 
   build: {
     lib: {
